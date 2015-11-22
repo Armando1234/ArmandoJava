2068259779a186dba22df77f1e9ecad203683182
Armando1234 <armanmartinezv@gmail.com>
2015-11-22 13:30:04 +0100

    calc

--- Ejercicios/src/Ejercicios/AmigoRobotico.java
+++ Ejercicios/src/Ejercicios/AmigoRobotico.java
@@ -8,12 +8,14 @@ public static void main(String[] args){
 		String message = "Cual es tu nombre?";
 		String nombre = JOptionPane.showInputDialog(null, message, "Info",JOptionPane.INFORMATION_MESSAGE);
 		String messageName = "Buenos dias, " + nombre;
-		String messagename2 = messageName + "\n Yo me llamo Armando";
+		String messagename2 = messageName + "\n Mucho gusto";
 		
 		JOptionPane.showMessageDialog (null, messagename2, "Info",JOptionPane.INFORMATION_MESSAGE);
 	
-	    String pregunta = "Eres del Barca o del Madrid?";
-	    String Equipo = JOptionPane.showInputDialog(null, pregunta, "Info",JOptionPane.INFORMATION_MESSAGE);
+	    String pregunta = "Como estas," + nombre + "?";
+	    String respuesta = JOptionPane.showInputDialog(null, pregunta, "Info",JOptionPane.INFORMATION_MESSAGE);
+	    
+	    }
 	
 	
 	
@@ -33,4 +35,4 @@ public static void main(String[] args){
 	
 }	
 	
-}
+

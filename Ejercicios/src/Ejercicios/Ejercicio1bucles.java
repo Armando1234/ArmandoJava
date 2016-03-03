74539d56d46175365159a4630b13342cd2beae1b
sss <m@m.com>
2016-03-03 19:31:12 +0100

    Bucles

---
+++ Ejercicios/src/Ejercicios/Ejercicio1bucles.java
@@ -0,0 +1,42 @@
+package Ejercicios;
+
+public class Ejercicio1bucles {
+
+	
+	public static void main (String [] args){
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		int num = Lib.getInt("introduce un numero entero por teclado", "Un numero!!");
+		int u = 1;
+		
+		
+		if(num < 1 * u){
+			
+			
+		}
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
